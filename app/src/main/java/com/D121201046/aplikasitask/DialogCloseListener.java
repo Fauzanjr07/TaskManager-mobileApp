cbce3b61a983dc8589866a143c46d717763e9940
Muh. Fauzan Azima <ffauzanazima@gmail.com>
2022-12-14 22:19:16 +0800

    Aplikasi Task Manager

---
+++ app/src/main/java/com/D121201046/aplikasitask/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.D121201046.aplikasitask;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
